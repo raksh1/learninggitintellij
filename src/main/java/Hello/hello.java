0d60e452f6fbe6f9368ab2ec38fcc4f5732d108d
hostbooks <@>
2022-08-06 16:42:35 +0530

    commit 2

--- src/main/java/Hello/hello.java
+++ src/main/java/Hello/hello.java
@@ -3,5 +3,10 @@ package Hello;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
+        int a=1;
+        int a1=2;
+        a=a1*2;
+        System.out.println(a);
+
     }
 }
